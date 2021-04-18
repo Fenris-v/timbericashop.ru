650c68bc05033608cb3ed980c64491be4da5a367
fenris <Fenris-v>
2021-04-18 12:22:12 +0300

    adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -25,15 +25,17 @@ $(document).ready(() => {
         arrows: true,
         dots: false,
         slidesToShow: 3,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4000,
         speed: 1000,
         prevArrow: '<div class="slick-arrow slick-prev"><img src="images/slider-arrow.png" alt="prev" /></div>',
         nextArrow: '<div class="slick-arrow slick-next"><img src="images/slider-arrow.png" alt="next" /></div>',
         responsive: [{
-            breakpoint: 767,
+            breakpoint: 900,
             settings: {
-                arrows: false
+                arrows: false,
+                dots: true,
+                slidesToShow: 2
             }
         }]
     });
